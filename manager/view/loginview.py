7646a6638d926be493184f453e88a18d8a4c9610
Startatsocialmind <461834084@qq.com>
2018-08-15 17:14:44 +0800

    Revert "LiRuixiang"
    This reverts commit 7016bfd

--- manager/view/loginview.py
+++ manager/view/loginview.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render,redirect
 from django.http import JsonResponse, HttpResponse
 from manager.scripts.sysscript import *
+import pymysql  # 导入 pymysql
+import json
 
 def index(request):
     return render(request, 'background/index.html')
 def login(request):
     return redirect('/logout/')
+def bindexContent(request):
+    data = {"data": "success"}
+    return HttpResponse(json.dumps(data))
 
