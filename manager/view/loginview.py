98404f40275f4ce241cd3693e4ef5c4a9ccee708
Startatsocialmind <461834084@qq.com>
2018-08-16 13:36:12 +0800

    Revert "LiRuixiang"
    This reverts commit 7016bfd

--- manager/view/loginview.py
+++ manager/view/loginview.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render,redirect
 from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from manager.scripts.sysscript import *
 import pymysql  # 导入 pymysql
+from visitor.models import User
+import time
 import json
 
 def index(request):
     return render(request, 'background/index.html')
 def login(request):
     return redirect('/logout/')
+@csrf_exempt
 def bindexContent(request):
-    data = {"data": "success"}
+    dataList = []
+    a =  User.objects.values("registranttime")
+    count = 0
+    LASTTIMEOFADMINLOGOUT = 1531885794.0
+    for i in a:
+        if i['registranttime']:
+            # print(i['registranttime'])
+            usrrgtime = time.mktime(time.strptime(i['registranttime'],"%Y-%m-%d %H:%M:%S"))
+            if usrrgtime>LASTTIMEOFADMINLOGOUT:
+                count = count + 1
+        else:
+            print("该用户注册时间为None")
+    dataList.append(count)
+    data = {"data": dataList}
     return HttpResponse(json.dumps(data))
 
